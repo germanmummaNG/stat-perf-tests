7485992f6ef3998d99f58f5dfa5e642c6250016d
German Mumma <german.mumma@netgroup.ee>
2019-07-17 12:04:34 +0300

    first commit

---
+++ locustfile.py
@@ -0,0 +1,30 @@
+from locust import HttpLocust, TaskSet, task
+
+class UserBehavior(TaskSet):
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.openDashboardRegionalStatistics()
+    
+    def openDashboardRegionalStatistics(self):
+        self.client.get("/dashboard/3")
+
+    @task
+    def openRegisteredUnemploymentBar(self):
+        self.client.get("/dashboard/3/widget/33?graphType=bar&language=et")
+
+    @task
+    def openRegisteredUnemploymentArea(self):
+        self.client.get("/dashboard/3/widget/33?graphType=area&language=et")
+
+    @task
+    def openRegisteredUnemploymentMap(self):
+        self.client.get("/dashboard/3/widget/33?graphType=map&language=et")
+
+    @task
+    def openRegisteredUnemploymentVertical(self):
+        self.client.get("/dashboard/3/widget/33?graphType=vertical&language=et")
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior
+    min_wait = 800
+    max_wait = 1200
