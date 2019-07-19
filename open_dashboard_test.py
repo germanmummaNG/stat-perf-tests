0e7772de3e368c46d20cc7aa350b9644c1ef8f8c
German Mumma <german.mumma@netgroup.ee>
2019-07-19 10:45:38 +0300

    add idea and sdk specific entries to gitignore

--- open_dashboard_test.py
+++ open_dashboard_test.py
@@ -1,18 +1,20 @@
-import random
 from locust import HttpLocust, TaskSet, task
+
 
 class UserBehavior(TaskSet):
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
         self.getAllDashboards()
-    
-    def getAllDashboards(self):
+
+    def get_all_dashboards(self):
         self.client.get("/dashboard")
 
     @task
-    def openDashboardRegionalStatistics(self):
+    def open_dashboard_regional_statistics(self):
         self.client.get("/dashboard/3")
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    wait_function = lambda self: random.expovariate(1)*1000
+    min_wait = 800
+    max_wait = 1200
--- switch_graph_types_test.py
+++ switch_graph_types_test.py
@@ -1,30 +1,32 @@
-import random
 from locust import HttpLocust, TaskSet, task
+
 
 class UserBehavior(TaskSet):
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
-        self.openDashboardRegionalStatistics()
-    
-    def openDashboardRegionalStatistics(self):
+        self.open_dashboard_regional_statistics()
+
+    def open_dashboard_regional_statistics(self):
         self.client.get("/dashboard/3")
 
     @task
-    def openRegisteredUnemploymentBar(self):
+    def open_registered_unemployment_bar(self):
         self.client.get("/dashboard/3/widget/33?graphType=bar&language=et")
 
     @task
-    def openRegisteredUnemploymentArea(self):
+    def open_registered_unemployment_area(self):
         self.client.get("/dashboard/3/widget/33?graphType=area&language=et")
 
     @task
-    def openRegisteredUnemploymentMap(self):
+    def open_registered_unemployment_map(self):
         self.client.get("/dashboard/3/widget/33?graphType=map&language=et")
 
     @task
-    def openRegisteredUnemploymentVertical(self):
+    def open_registered_unemployment_vertical(self):
         self.client.get("/dashboard/3/widget/33?graphType=vertical&language=et")
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
-    wait_function = lambda self: random.expovariate(1)*1000
+    min_wait = 800
+    max_wait = 1200
